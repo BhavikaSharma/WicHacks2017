8a11eecf193084b324dab87732ae4bd706bc1c9c
Elaine Ye <elaineye983@gmail.com>
2017-02-26 06:28:16 +0000

    elaine commit!

---
+++ amplify.js
@@ -0,0 +1,2 @@
+localStorage.setItem('room',document.getElementById('roomNum').value)
+localStorage.setItem('pwd',document.getElementById('roomPwd').value)
---
+++ room_data.js
@@ -0,0 +1,5 @@
+global.localStorage.setItem("roomNum", document.getElementById("roomNum").value);
+global.localStorage.setItem("roomPwd", document.getElementById("roomPwd").value);
+
+global.localStorage.setItem("roomVerify", document.getElementById("roomVerify").value);
+global.localStorage.setItem("pwdVerify", document.getElementById("pwdVerify").value);
